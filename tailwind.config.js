16a2930b3b3e220d1f01b465fe37e66c88734de2
Jang Hee Won <jhw001013@kyonggi.ac.kr>
2025-08-08 14:47:12 +0900

    chore/tailwind 세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,28 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Pretendard", "ui-sans-serif", "system-ui"],
+        pretendard: ["Pretendard", "sans-serif"],
+      },
+      Shadow: {
+        side: "0px 8px 28px rgba(1, 5, 17, 0.3)",
+      },
+      brand: {
+        labela: "#cb3cff",
+        labelb: "#0038ff",
+        defect: "#00c2ff",
+        day: "#7F25FB",
+        bg: "#081028",
+        box: "#0B1739",
+        Listsepar: "#1A1B2F",
+        border: "#343B4F",
+        nav: "#081028",
+        button: "#cb3cff",
+      },
+      textColor: {
+        heading: "#FFFFFF",
+        sub: "#aeb9e1",
+        gradea: "#14CA74",
+        gradeb: "#FFCE20",
       },
     },
   },
